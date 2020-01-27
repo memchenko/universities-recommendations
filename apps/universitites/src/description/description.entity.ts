e49e666c5fa2032714ee885f23462efbffb507e1
Mikhail Emchenko <mikhail.emchenko@gmail.com>
2020-01-27 23:39:51 +0400

    feat(universities): added types to description module

--- apps/universitites/src/description/description.entity.ts
+++ apps/universitites/src/description/description.entity.ts
@@ -1,10 +1,12 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+import { IDescriptionEntity } from './types';
+
 @Entity('description')
-export default class DescriptionEntity {
+export default class DescriptionEntity implements IDescriptionEntity {
     @PrimaryGeneratedColumn()
     public id!: number;
 
     @Column({ type: 'varchar' })
-    public description!: string;
+    public value!: string;
 }
---
+++ apps/universitites/src/description/types.ts
@@ -0,0 +1,4 @@
+export interface IDescriptionEntity {
+    id: number;
+    value: string;
+}
