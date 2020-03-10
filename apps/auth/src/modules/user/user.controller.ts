4f7f8f77d51975918ac5037b72bd384c11401be3
Mikhail Emchenko <mikhail.emchenko@gmail.com>
2020-03-10 23:40:32 +0400

    feat(auth): added method to get many users

--- apps/auth/src/modules/user/user.controller.ts
+++ apps/auth/src/modules/user/user.controller.ts
@@ -1,80 +1,44 @@
-import { Controller, UseGuards, Get, Request, NotFoundException } from '@nestjs/common';
-import { Repository, } from 'typeorm';
+import { Controller, UseGuards, Get, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
-import { lensProp, view, ifElse, isNil, T, identity, omit } from 'ramda';
-
-import UserEntity from './user.entity';
 import { IUserEntity } from './types';
-import SettingEntity from '../setting/setting.entity';
 
 import {
   USER,
   USERS,
   USER_CONTACT,
   USER_CONTACTS,
-  USER_ROLE,
-  USER_ROLES,
 } from '../../constants/routes';
-import { ContactType } from '../../constants/entities';
 import { FastifyRequestWithJWTUser } from '../auth/types';
-import ContactEntity from '../contact/contact.entity';
+import UserService from './user.service';
+import { FastifyRequest } from 'fastify';
 
-const isEmailVisibleLens = lensProp('setting_is_email_visible');
-const isPhoneVisibleLens = lensProp('setting_is_phone_visible');
-const normalizeSetting = ifElse(isNil, T, identity);
-const getSafeSettingValue = (lens, data) => normalizeSetting(view(lens, data))
 
 @Controller()
 @UseGuards(AuthGuard('jwt'))
 export default class UserController {
   constructor(
-    @InjectRepository(UserEntity)
-    private readonly users: Repository<UserEntity>,
+    private usersService: UserService,
   ) {}
 
   @Get(USER)
-  public async getUser(
+  public getUser(
     @Request() req: FastifyRequestWithJWTUser,
   ): Promise<Omit<IUserEntity, 'password'>> {
-    const { raw, entities } = await this.users.createQueryBuilder('user')
-      .where({ id: req.user.id })
-      .leftJoinAndSelect(ContactEntity, 'contact', '"user"."id" = "contact"."user_id"')
-      .leftJoinAndSelect(SettingEntity, 'setting', '"user"."id" = "setting"."user_id"')
-      .getRawAndEntities();
-
-    if (!entities.length) {
-      throw new NotFoundException();
-    }
-
-    const contactsVisibility = {
-      [ContactType.Email]: getSafeSettingValue(isEmailVisibleLens, raw[0]),
-      [ContactType.Phone]: getSafeSettingValue(isPhoneVisibleLens, raw[0]),
-    };
-    
-    entities[0].contacts = raw
-      .filter((result) => !!contactsVisibility[result['contact_contact_type']])
-      .map((result) => ({
-        id: result['contact_id'],
-        contactType: result['contact_contact_type'],
-        value: result['contact_value'],
-      }));
-  
-    return omit(['password'], entities[0]);
+    return this.usersService.getUser(req.user.id);
   }
 
   @Get(USERS)
-  public getUsers() {}
+  public getUsers(
+    @Request() req: FastifyRequest,
+  ): Promise<Omit<IUserEntity, 'password'>[]> {
+    const { limit, offset } = req.query;
+
+    return this.usersService.getUsers(limit, offset);
+  }
 
   @Get(USER_CONTACT)
   public getUserContact() {}
 
   @Get(USER_CONTACTS)
   public getUserContacts() {}
-
-  @Get(USER_ROLE)
-  public getUserRole() {}
-
-  @Get(USER_ROLES)
-  public getUserRoles() {}
 }
--- apps/auth/src/modules/user/user.service.ts
+++ apps/auth/src/modules/user/user.service.ts
@@ -1,16 +1,131 @@
-import { Injectable } from '@nestjs/common';
-import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { lensProp, ifElse, isNil, T, identity, view, omit, compose } from 'ramda';
 
 import UserEntity from './user.entity';
+import ContactEntity from '../contact/contact.entity';
+import SettingEntity from '../setting/setting.entity';
+
+import { ContactType } from '../../constants/entities';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { IUserEntity } from './types';
+
+const getSafeSettingValue = compose(
+  ifElse(
+    isNil,
+    T,
+    identity,
+  ),
+  view,
+);
+
+const SETTINGS_LENSES = [
+  [ContactType.Email, 'setting_is_email_visible'],
+  [ContactType.Phone, 'setting_is_phone_visible'],
+].map((entry) => ([
+  entry[0],
+  lensProp(entry[1])
+]));
 
 @Injectable()
 export default class UserService extends TypeOrmCrudService<UserEntity> {
+  private DEFAULT_PAGE_SIZE = 10;
+
   constructor(
     @InjectRepository(UserEntity)
-    readonly repository: Repository<UserEntity>,
+    readonly users: Repository<UserEntity>,
   ) {
-    super(repository);
+    super(users);
+  }
+
+  public async getUser(userId: number): Promise<Omit<IUserEntity, 'password'>> {
+    const { raw, entities } = await this.users.createQueryBuilder('user')
+      .where({ id: userId })
+      .leftJoinAndSelect(ContactEntity, 'contact', '"user"."id" = "contact"."user_id"')
+      .leftJoinAndSelect(SettingEntity, 'setting', '"user"."id" = "setting"."user_id"')
+      .getRawAndEntities();
+
+    if (!entities.length) {
+      throw new NotFoundException();
+    }
+
+    const user = entities[0];
+    const contactsVisibility = this.getContactsVisibility(user, raw);
+    
+    this.addContactsToUser(user, raw, contactsVisibility);
+  
+    return omit(['password'], user);
+  }
+
+  public async getUsers(
+    limit: number = this.DEFAULT_PAGE_SIZE,
+    offset: number = 0
+  ): Promise<Omit<IUserEntity, 'password'>[]> {
+    const { raw, entities } = await this.users.createQueryBuilder('user')
+      .leftJoinAndSelect(ContactEntity, 'contact', '"user"."id" = "contact"."user_id"')
+      .leftJoinAndSelect(SettingEntity, 'setting', '"user"."id" = "setting"."user_id"')
+      .take(limit)
+      .skip(offset)
+      .getRawAndEntities();
+
+    if (!entities.length) {
+      return [];
+    }
+
+    return entities.map((user) => {
+      const contactsVisibility = this.getContactsVisibility(user, raw);
+
+      this.addContactsToUser(user, raw, contactsVisibility);
+
+      return omit(['password'], user);
+    });
+  }
+
+  private getContactsVisibility(
+    user: Omit<IUserEntity, 'contacts'>,
+    rawSQLResult: {
+      user_id: number,
+      setting_is_email_visible: boolean,
+      setting_is_phone_visible: boolean,
+    }[]
+  ): { [key: string]: boolean } {
+    const userSettings = rawSQLResult
+      .filter((result) => result['user_id'] === user.id)[0];
+
+    return SETTINGS_LENSES.reduce((acc, entry) => {
+      acc[entry[0]] = getSafeSettingValue(entry[1], userSettings);
+      return acc;
+    }, {});
+  }
+
+  private addContactsToUser(
+    user: Omit<IUserEntity, 'contacts'>,
+    rawSQLResult: {
+      contact_contact_type: ContactType,
+      contact_id: number,
+      contact_value: string,
+      user_id: number,
+    }[],
+    contactsVisibility: { [key: string]: boolean, },
+  ): IUserEntity {
+    console.log(rawSQLResult);
+
+    const res: IUserEntity = Object.assign(
+      user,
+      {
+        contacts: rawSQLResult
+          .filter((result) => (
+            result['user_id'] === user.id
+            && !!contactsVisibility[result['contact_contact_type']]))
+          .map((result) => ({
+            id: result['contact_id'],
+            contactType: result['contact_contact_type'],
+            value: result['contact_value'],
+          }))
+      }
+    );
+
+    return res;
   }
 }
